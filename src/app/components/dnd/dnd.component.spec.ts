a52b12158cf0120a9d24e5386d066c864df40dda
Rumen Neshev <rstar2@abv.bg>
2017-07-05 21:25:47 +0300

    fix for 2angular/cli

--- src/app/components/dnd/dnd.component.spec.ts
+++ src/app/components/dnd/dnd.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { DnDComponent } from './dn-d.component';
+import { DnDComponent } from './dnd.component';
 
 describe('DnDComponent', () => {
   let component: DnDComponent;
